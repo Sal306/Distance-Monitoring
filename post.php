7f1bcb697346fc570653197eb03fd63aab1ebc98
Salih Saad <ss17039@auis.edu.krd>
2021-03-06 17:19:06 +0300

    initial release

---
+++ post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
+<?php
+// $data = $_POST["temp"];
+// echo $data;
+
+echo ('YES');
+ ?>
