c17786b10553895c3a6b492c64d6edb4169ad4c0
Salih Saad <ss17039@auis.edu.krd>
2021-03-27 22:08:18 +0300

    added db

--- db_connect.php
+++ db_connect.php
@@ -10,7 +10,7 @@
 
     // $conn = pg_connect(getenv("postgres://fqlsipmoauptdn:aeaf5065cafd4422a1c9c8f086c362906c2275f6d4e1f9f45b1082a7b8643fe9@ec2-107-22-245-82.compute-1.amazonaws.com:5432/dcqeloql3v0p9s"));
     $dsn = "pgsql:"
-    . "ec2-107-22-245-82.compute-1.amazonaws.com;"
+    . "host=ec2-107-22-245-82.compute-1.amazonaws.com;"
     . "dbname=dcqeloql3v0p9s;"
     . "user=fqlsipmoauptdn;"
     . "port=5432;"
