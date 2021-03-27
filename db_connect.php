5f1ac98e588a14430c0ae87e061ce093c5d84c38
Salih Saad <ss17039@auis.edu.krd>
2021-03-27 16:20:49 +0300

    added db

--- db_connect.php
+++ db_connect.php
@@ -8,7 +8,8 @@
 
     <?php
 
-    $conn = pg_connect(getenv("postgres://viyzxonpnmgcit:42e476a1e406d20a3f33c8a01d0b15de8e77c5e37d4b50a9a788d4a7161c6c4d@ec2-52-1-115-6.compute-1.amazonaws.com:5432/d30juj5p9l7hdh"));
+    $conn = pg_connect(getenv("postgres://fqlsipmoauptdn:aeaf5065cafd4422a1c9c8f086c362906c2275f6d4e1f9f45b1082a7b8643fe9@ec2-107-22-245-82.compute-1.amazonaws.com:5432/dcqeloql3v0p9s
+"));
 
     if($conn){
       echo "Connected ";
