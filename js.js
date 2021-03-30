84adc896273e713dc1699b2f69e3705e3803740f
Salih Saad <ss17039@auis.edu.krd>
2021-03-30 20:26:44 +0300

    Arch 1st

--- js.js
+++ js.js
@@ -5,8 +5,14 @@ let fullStar = document.querySelector(".imgfull");
 // get images' container
 let starContainer = document.querySelector(".star_container");
 
-// while clicking, swap the disable class 
+
+alert(hi);
+console.log(fullStar)
+
+// while clicking, swap the disable class
 starContainer.addEventListener("click", function () {
+  alert(hi);
+  console.log(fullStar)
   emptyStar.classList.toggle("disable");
   fullStar.classList.toggle("disable");
 });
