1a155bea7eb3196a45dd33549f75670a608a7d3d
Salih Saad <ss17039@auis.edu.krd>
2021-03-31 18:53:36 +0300

    add fav

--- index.php
+++ index.php
@@ -73,57 +73,57 @@ $.ajax({
 													 "<div>"  +
 													 			"<span class= 'serv__status serv__status--up'>" +
 																			"&#9679;" +
-																 "</span">	+
-																 // "<h3 class='serv__name'> " + 	id +
-																 //
-																	// "</h3>" ;
+																 "</span>"	+
+																 "<h3 class='serv__name'> " + 	loc +
+
+																	"</h3>" ;
 													 "</div>" +
 
 													 "<div class='server__ip'>"  +
 													 			"<span>" ;
-											// 				 			"<div class='star_container" + id +  "'>" +
-											// 										"<img src='img/star_full.png' class='imgfull" + id + " disable'>" +
-											// 										"<img src='img/star_empty.png' class='imgempty" + id + "'>" +
-											// 							"</div>" +
-											// 							"DateValue" +
-											// 					"</span>" +
-											// 		  "</div>" +
-										 // "</header>" +
+															 			"<div class='star_container" + id +  "'>" +
+																					"<img src='img/star_full.png' class='imgfull" + id + " disable'>" +
+																					"<img src='img/star_empty.png' class='imgempty" + id + "'>" +
+																		"</div>" +
+																		"DateValue" +
+																"</span>" +
+													  "</div>" +
+										 "</header>" +
 
 
-							//  "<article class='serv__gpu'>" +
-							//  	"<div class='serv__grid-3'>" +
-							//  	"	<div class='serv__mod'>" +
-							//  		"	<div class='serv__modLabel serv__modLabel--red'>"+
-							//  				"Distance" +
-							//  			"</div>"+
-							//  		"	<div class='serv__modNumber'>"
-							//  			 + distance +"<label class='serv__modUnit'>M</label>" +
-							//  		"	</div>"+
-							//  	"	</div>" +
-							//
-							//  	"	<div class='serv__mod'>" +
-							//  		"	<div class='serv__modLabel serv__modLabel--green'>" +
-							//  				"Temperature: " +
-							//  			"</div>" +
-							//  			"<div class='serv__modNumber'>" +
-							//  				temp + "<label class='serv__modUnit'>&deg;C</label>" +
-							//  		"	</div>"+
-							//  		"</div>"+
-							//
-							//  	"	<div class='serv__mod'>"+
-							//  			"<div class='serv__modLabel serv__modLabel--yellow'>"+
-							//  				"Time"+
-							//  			"</div>"+
-							//  			"<div class='serv__modNumber'>" +
-							//  				"TimeValue<label class='serv__modUnit'></label>" +
-							//  		"	</div>"+
-							//  		"</div>"+
-							//
-							//
-							//  	"</div>"+
-							//  "</article>"  +
-							// "</section>";
+							 "<article class='serv__gpu'>" +
+							 	"<div class='serv__grid-3'>" +
+							 	"	<div class='serv__mod'>" +
+							 		"	<div class='serv__modLabel serv__modLabel--red'>"+
+							 				"Distance" +
+							 			"</div>"+
+							 		"	<div class='serv__modNumber'>"
+							 			 + distance +"<label class='serv__modUnit'>M</label>" +
+							 		"	</div>"+
+							 	"	</div>" +
+
+							 	"	<div class='serv__mod'>" +
+							 		"	<div class='serv__modLabel serv__modLabel--green'>" +
+							 				"Temperature: " +
+							 			"</div>" +
+							 			"<div class='serv__modNumber'>" +
+							 				temp + "<label class='serv__modUnit'>&deg;C</label>" +
+							 		"	</div>"+
+							 		"</div>"+
+
+							 	"	<div class='serv__mod'>"+
+							 			"<div class='serv__modLabel serv__modLabel--yellow'>"+
+							 				"Time"+
+							 			"</div>"+
+							 			"<div class='serv__modNumber'>" +
+							 				"TimeValue<label class='serv__modUnit'></label>" +
+							 		"	</div>"+
+							 		"</div>"+
+
+
+							 	"</div>"+
+							 "</article>"  +
+							"</section>";
 							 alert(tr_str);
 
                     $(".contianer").append(tr_str);
