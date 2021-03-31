5198bdda6112ffc136fc9f0f8177e0c58fb8cdae
Salih Saad <ss17039@auis.edu.krd>
2021-03-31 18:24:46 +0300

    fav add

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ $.ajax({
                 var len = response.length;
                 for (var i = 0; i < len; i++) {
                     id = response[i].id;
-                    // location = response[i].location;
+                    loc = response[i].location;
                   	temp = response[i].temp;
                     distance = response[i].distance;
 
