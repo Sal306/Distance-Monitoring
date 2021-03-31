0e5bec1e003577fe81e2bb8a47e8f4daa911c904
Salih Saad <ss17039@auis.edu.krd>
2021-03-31 21:20:27 +0300

    fav backend

--- index.php
+++ index.php
@@ -134,18 +134,7 @@ $.ajax({
         });
     });
 
-		let emptyStar = document.querySelector(".imgempty1 ");
-		let fullStar = document.querySelector(".imgfull1");
-
-		// get images' container
-		let starContainer = document.querySelector(".star_container");
-
-		// while clicking, swap the disable class
-		starContainer.addEventListener("click", function () {
-		  console.log(fullStar);
-		  emptyStar.classList.toggle("disable");
-		  fullStar.classList.toggle("disable");
-		});
+		
 
 </script>
 </html>
