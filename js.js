8a8027775b5cf453ef11ec98300c38b8efd4ba27
Salih Saad <ss17039@auis.edu.krd>
2021-03-31 21:34:35 +0300

    fav backend

--- js.js
+++ js.js
@@ -1,15 +1,15 @@
-$('document').ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
+  let emptyStar = document.querySelector(".imgempty1");
+  let fullStar = document.querySelector(".imgfull1");
+
+  // get images' container
+  let starContainer = document.querySelector(".star_container1");
+
+  // while clicking, swap the disable class
+  starContainer.addEventListener("click", function () {
+    console.log(fullStar);
+    emptyStar.classList.toggle("disable");
+    fullStar.classList.toggle("disable");
+  });
+});
 // get images elements
-let emptyStar = document.querySelector(".imgempty1");
-let fullStar = document.querySelector(".imgfull1");
-
-// get images' container
-let starContainer = document.querySelector(".star_container1");
-
-// while clicking, swap the disable class
-starContainer.addEventListener("click", function () {
-  console.log(fullStar);
-  emptyStar.classList.toggle("disable");
-  fullStar.classList.toggle("disable");
-});
-});
