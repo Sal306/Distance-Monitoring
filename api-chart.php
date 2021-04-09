0d983a2c52321e87615a52b2788c60c798a0f1ce
Salih Saad <ss17039@auis.edu.krd>
2021-04-10 00:29:40 +0300

    replaced location with hum

---
+++ api-chart.php
@@ -0,0 +1,32 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+
+define('TITLE', 'Report');
+
+require_once('db_connect.php');
+
+//array to hold the returned values from the DB
+$return_arr = array();
+
+//query to return the values
+$result = $db->query("select * from (select id, case
+when date > now() - interval '1 week'  then 'week1' 
+when date > now() - interval '2 week' then 'week2'
+when date > now() - interval '3 week' then 'week3' 
+when date > now() - interval '4 week' then 'week4' 
+end
+from report ) as A where A.case !='null';");
+
+while ($row = $result->fetch()) {
+  $id = $row['id'];
+  $case = $row['case'];
+  
+    $return_arr[] = array( 
+      "id" => $id,
+      "case" => $case
+  );
+}
+
+//return the json encoded values
+echo json_encode($return_arr);
