ed0e23bb63812432872b772a5d793ec3546382f0
Salih Saad <ss17039@auis.edu.krd>
2021-04-12 02:14:14 +0300

    final touches

---
+++ api_ShowFav.php
@@ -0,0 +1,31 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+
+define('TITLE', 'Report');
+
+require_once('db_connect.php');
+
+//array to hold the returned values from the DB
+$return_arr = array();
+
+
+  $result = $db->query("select date, temp, distance, humidity  from 
+report where fav ='true'");
+
+while ($row = $result->fetch()) {
+    $week4 = $row['avg'];
+
+
+    $return_arr[] = array( 
+        "date" => $date,
+        "dist" => $dist,
+        "hum" => $hum,
+        "temp" => $temp
+        
+    ); }
+  
+ 
+
+//return the json encoded values
+echo json_encode($return_arr);
