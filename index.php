acf9186d2b1305f2e709762ff3fdec340cf66dad
Salih Saad <ss17039@auis.edu.krd>
2021-04-11 23:44:12 +0300

    added fav and chart html pages

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<title>AUIS Monitoring System</title>
 	<link rel="stylesheet" type="text/css" href="ss.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="https://github.com/TixieBorg/Ethereum-panel/blob/6797e8e7aef50453b1e1236414c31300f7abf37f/css/design.css">
 </head>
 <body>
