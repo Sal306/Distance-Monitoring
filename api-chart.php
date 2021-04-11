a5a3178575a42d22466fcfccde0abb0e173e0f71
Salih Saad <ss17039@auis.edu.krd>
2021-04-11 04:43:42 +0300

    making chart

--- api-chart.php
+++ api-chart.php
@@ -10,23 +10,65 @@ require_once('db_connect.php');
 $return_arr = array();
 
 //query to return the values
-$result = $db->query("select * from (select distance, case
+$result = $db->query("select AVG(distance) from (select distance, case
 when date > now() - interval '1 week'  then 'week1' 
 when date > now() - interval '2 week' then 'week2'
 when date > now() - interval '3 week' then 'week3' 
 when date > now() - interval '4 week' then 'week4' 
 end
-from report ) as A where A.case !='null'");
+from report ) as A where A.case ='week1'");
+
 
 while ($row = $result->fetch()) {
-  $dist = $row['distance'];
-  $case = $row['case'];
-  
-    $return_arr[] = array( 
-      "dist" => $dist,
-      "case" => $case
-  );
+  $week1 = $row['avg'];
 }
+
+$result = $db->query("select AVG(distance) from (select distance, case
+when date > now() - interval '1 week'  then 'week1' 
+when date > now() - interval '2 week' then 'week2'
+when date > now() - interval '3 week' then 'week3' 
+when date > now() - interval '4 week' then 'week4' 
+end
+from report ) as A where A.case ='week2'");
+
+while ($row = $result->fetch()) {
+    $week2 = $row['avg'];
+  }
+
+
+  $result = $db->query("select AVG(distance) from (select distance, case
+when date > now() - interval '1 week'  then 'week1' 
+when date > now() - interval '2 week' then 'week2'
+when date > now() - interval '3 week' then 'week3' 
+when date > now() - interval '4 week' then 'week4' 
+end
+from report ) as A where A.case ='week3'");
+
+while ($row = $result->fetch()) {
+    $week3 = $row['avg'];
+  }
+
+  $result = $db->query("select AVG(distance) from (select distance, case
+when date > now() - interval '1 week'  then 'week1' 
+when date > now() - interval '2 week' then 'week2'
+when date > now() - interval '3 week' then 'week3' 
+when date > now() - interval '4 week' then 'week4' 
+end
+from report ) as A where A.case ='week4'");
+
+while ($row = $result->fetch()) {
+    $week4 = $row['avg'];
+
+
+    $return_arr[] = array( 
+        "bar1" => $week1,
+        "bar2" => $week2,
+        "bar3" => $week3,
+        "bar4" => $week4
+        
+    ); }
+  
+ 
 
 //return the json encoded values
 echo json_encode($return_arr);
