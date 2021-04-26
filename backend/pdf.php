4754a1e8e7a072a67f6b2612a543cf1a2efaa56f
Salih Saad <ss17039@auis.edu.krd>
2021-04-26 10:01:06 +0300

    export as pdf functionality

---
+++ backend/pdf.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+
+define('TITLE', 'Report');
+
+require_once('db_connect.php');
+require_once __DIR__ . '/vendor/autoload.php';
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$data = $data['data'];
+
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->WriteHTML($data);
+$mpdf->Output();
