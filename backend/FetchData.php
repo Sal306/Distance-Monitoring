1fa2a5b42a8e76e52933c55fe78a57a77dda44ca
Salih Saad <ss17039@auis.edu.krd>
2021-05-09 03:19:04 +0300

    s

--- backend/FetchData.php
+++ backend/FetchData.php
@@ -2,6 +2,7 @@
 // this file will recieve the post request and send it to temp.html 
 // the data will be formatted like that <div id='var'> , then from the temp.html. the data will be send to db  
 $data = $_POST["dist"];
+//$date = "<div id='hum'> 15 </div>";
 
 
 
