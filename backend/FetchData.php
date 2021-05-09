d0c8b91efeff04c7e76b64193693d3929a8cf7b3
Salih Saad <ss17039@auis.edu.krd>
2021-05-09 05:19:32 +0300

    s

--- backend/FetchData.php
+++ backend/FetchData.php
@@ -4,22 +4,7 @@
     $data = $_POST["dist"];
     $file = 'temp.html';    
     file_put_contents($file, $data);
-    
+    $newcontent = file_get_contents("temp.html");
+    echo $newcontent;
     ?>
 
-      var x = <?php $data ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-<script>
-  alert('hi');
-</script>
-</html>
