ea0a663880525bf5fa56450e88fbb2c7b2d5217a
Salih Saad <ss17039@auis.edu.krd>
2021-05-09 03:34:32 +0300

    s

--- backend/FetchData.php
+++ backend/FetchData.php
@@ -9,18 +9,4 @@ $file = "/frontend/temp.html";
 file_put_contents($file, $data);
 
 
- ?>
- <!DOCTYPE html>
- <html lang="en">
- <head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
- </head>
- <body>
-     <?php
- echo $data;
-?>
- </body>
- </html>
+ 
