7ecf47916ccee88d70ee51d6fedff6f165501812
Salih Saad <ss17039@auis.edu.krd>
2021-05-09 03:50:56 +0300

    s

--- backend/FetchData.php
+++ backend/FetchData.php
@@ -9,6 +9,16 @@
 </head>
 <body> <?php
     echo $data;
+    $data = $_POST["dist"];
+    $file = "/frontend/temp.html";
+
+    file_put_contents($file, $data);
+    echo $data;
+    $data = 'salih';
+    echo $data;
+
+$file = "/frontend/temp.html";
+
     ?>
 </body>
 </html>
@@ -17,10 +27,6 @@
  <?php
 // this file will recieve the post request and send it to temp.html 
 // the data will be formatted like that <div id='var'> , then from the temp.html. the data will be send to db  
-$data = $_POST["dist"];
-
-
-$file = "/frontend/temp.html";
 
 file_put_contents($file, $data);
 ?> <!DOCTYPE html>
