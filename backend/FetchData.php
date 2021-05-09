841e5593adf472a623d9095f20c2abbfdd996095
Salih Saad <ss17039@auis.edu.krd>
2021-05-09 04:48:51 +0300

    s

--- backend/FetchData.php
+++ backend/FetchData.php
@@ -1,8 +1,11 @@
 
 <?php
-    $data = $_POST["dist"];
-    $file = 'temp.html';
+$count =0;
 
+    $data = $_POST["dist"];
+    if($file){
+    $file = 'temp.html';
+    }
     file_put_contents($file, $data);
 
 
