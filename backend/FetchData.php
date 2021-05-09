4d2c89ff7447fb369ceede5b6b2686a30e8c119e
Salih Saad <ss17039@auis.edu.krd>
2021-05-09 03:38:14 +0300

    s

--- backend/FetchData.php
+++ backend/FetchData.php
@@ -1,17 +1,5 @@
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
- </head>
- <body> <?php
-     echo $data;
-     ?>
- </body>
- </html>
+
  <?php
 // this file will recieve the post request and send it to temp.html 
 // the data will be formatted like that <div id='var'> , then from the temp.html. the data will be send to db  
@@ -21,4 +9,16 @@ $data = $_POST["dist"];
 $file = "/frontend/temp.html";
 
 file_put_contents($file, $data);
-?>
+?> <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> <?php
+    echo $data;
+    ?>
+</body>
+</html>
