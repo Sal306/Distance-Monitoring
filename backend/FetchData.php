afb4605b9cd0b0348283c404f81152c61efc2ce9
Salih Saad <ss17039@auis.edu.krd>
2021-05-09 03:45:23 +0300

    s

--- backend/FetchData.php
+++ backend/FetchData.php
@@ -1,4 +1,18 @@
 
+> <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> <?php
+    echo $data;
+    ?>
+</body>
+</html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
  <?php
 // this file will recieve the post request and send it to temp.html 
@@ -22,47 +36,4 @@ file_put_contents($file, $data);
     ?>
 </body>
 </html>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<script>
-   $(document).ready(function() {
-     // retrieve the values 
-   let hum = document.getElementById('hum'); 
-   let dist = document.getElementById('dist');
-   let temp = document.getElementById('temp');
-    
-   hum = hum.innerHTML;
-   dist = dist.innerHTML;
-   temp = temp.innerHTML;
-   
-  
-   var date = new Date(dateTime. getTime());
-   date.setHours(0, 0, 0, 0);
-   
-
-   obj = {
-     'hum': hum, 
-     'dist': dist,
-     'temp' : temp,
-     'date' : date
-   };
-   var json = JSON.stringify(obj);
-        //passing the values using ajax
-        var options = {
-            url: "/backend/insert_db.php",
-            dataType: "json",
-            type: "POST",
-            data: json,
-
-            success: function(xhr, status, error) {
-                alert(success);
-            },
-            error: function(xhr, status, error) {
-                alert("error");
-            }
-        };
-        //sending the ajax
-        $.ajax(options);
-      });
-
-</script>
